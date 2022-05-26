d295a13fef4dc8f652a1977ad3b87eddedffb4fc
Hasyim Sri Wahyudi <hasyimsriewahyudi@gmail.com>
2022-05-27 06:01:54 +0700

    update file

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,6 +4,7 @@ import Container from './Container';
 export default function Navbar() {
     const [dropdown, setDropdown] = useState(false);
     const [offcanvas, setOffcanvas] = useState(false);
+    const [search, setSearch] = useState(false);
     const dropdownList = [
     { text: 'Internet', href: '#' },
     { text: 'Books', href: '#' },
@@ -14,7 +15,7 @@ export default function Navbar() {
             <Container>
                 <div className='flex items-center'>
                     <div className='w-3/12 lg:hidden'>
-                        <button onClick={() => { setOffcanvas(!offcanvas)} }>
+                        <button onClick={() => setOffcanvas(!offcanvas) }>
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
@@ -25,17 +26,19 @@ export default function Navbar() {
                         Epictetus
                     </div>
                     <div className='w-3/12 lg:hidden text-right'>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="inline-block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                        </svg>
+                        <button onClick={() => setSearch(!search) }>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="inline-block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                            </svg>
+                        </button>
                     </div>
-                    <div className={`lg:w-7/12 w-full fixed bg-gradient-to-b from-gray-600 to-gray-900 top-0 h-full p-10 transition-all duration-500 ${offcanvas ? 'left-0' : '-left-full'}`}>
-                        <button className='absolute top-5 right-5' onClick={() => { setOffcanvas(false) }}>
+                    <div className={`lg:w-7/12 w-full fixed lg:static lg:bg-none bg-gradient-to-b from-gray-600 to-gray-900 lg:h-auto lg:p-0 top-0 h-full p-10 transition-all duration-500 ${offcanvas ? 'left-0' : '-left-full'}`}>
+                        <button className='absolute top-5 right-5 lg:hidden' onClick={() => setOffcanvas(false) }>
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                             </svg>
                         </button>
-                        <ul className='flex lg:items-center lg:space-x-14 flex-col space-y-4'>
+                        <ul className='flex lg:items-center lg:space-x-14 flex-col space-y-4 lg:flex-row lg:space-y-0'>
                             <li><a href='#' className='hover:underline'>UI Design</a></li>
                             <li><a href='#' className='hover:underline'>Front-End</a></li>
                             <li><a href='#' className='hover:underline'>Back-End</a></li>
@@ -57,8 +60,13 @@ export default function Navbar() {
                             </li>
                         </ul>
                     </div>
-                    <div className='w-3/12 hidden'>
-                        <input className="py-2 px-5 bg-gray-800 w-full rounded-full" placeholder='Search..' />
+                    <div className={`lg:w-3/12 absolute w-full left-0 px-8 lg:static lg:px-0 transition-all duration-500 ${search ? 'top-10' : '-top-40'}`}>
+                        <button className='absolute top-2 right-9 lg:hidden' onClick={() => setSearch(false) }>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                        <input className="py-2 px-5 bg-gray-800 w-full lg:rounded-full rounded-xl" placeholder='Search..' />
                     </div>
                 </div>
             </Container>
